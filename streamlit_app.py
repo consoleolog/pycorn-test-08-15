a12f4f67a6d045b281df700391db0179ab7c14c9
consoleolog <167042699+consoleolog@users.noreply.github.com>
2025-08-15 05:59:57 +0000

    feat: first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Custom App")
+
+st.info("streamlit 튜토리얼")
+st.subheader("sub header")
+
+
+st.link_button("네이버", "naver.com")
